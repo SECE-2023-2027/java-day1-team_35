4265ca4288307e372e2190380b7e9ca05273b6f0
darsansece <darsan.s2023csbs@sece.ac.in>
2026-06-15 19:31:46 +0530

    q3

---
+++ q3.java
@@ -0,0 +1,11 @@
+public class q3 {
+    public static void main(String[] args) {
+
+        double radius = 7.5;
+        double area = Math.PI * radius * radius;
+        double perimeter = 2 * Math.PI * radius;
+
+        System.out.println("Area = " + area);
+        System.out.println("Perimeter = " + perimeter);
+    }
+}
