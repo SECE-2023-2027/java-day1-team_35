3e1e6d8f2e3399eca1cd13fd29302d7e03fc3897
mohanprasathcsbs <mohanprasath.n2023csbs@sece.ac.in>
2026-06-15 19:05:00 +0530

    q12

---
+++ Q12.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Q12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input a character: ");
+        char ch = sc.next().charAt(0);
+
+        int ascii = (int) ch;
+
+        System.out.println("The ASCII value of " + ch + " is :" + ascii);
+
+        sc.close();
+    }
+}
